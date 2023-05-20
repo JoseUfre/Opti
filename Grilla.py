d357e2302fdccf4bc6c71fddfdc943a35423532f
JOSE UFRE <jose.ufre@uc.cl>
2023-05-19 21:02:45 -0400

    conjunto vecinos

--- Grilla.py
+++ Grilla.py
@@ -1,9 +1,9 @@
-from auxiliares import no_sale, riega
+from auxiliares import no_sale, riega, vecino
 from random import randint, seed
 
-filas = 8
-columnas = 8
-radios = (1, 3, 5, 7)
+filas = 5
+columnas = 5
+radios = (1, 5, 7)
 
 grid = [[c for c in range(columnas)] for f in range(filas)]
 
@@ -16,24 +16,33 @@ C_hl = {}
 for r in radios:
     C_hl[r] = (set(range(r, filas - 1 - r)), set(range(r, columnas - 1 - r)))
 
+# -----------------------------------------------------------------------------------------
+# Conjunto que tiene las listas de los puntos (h,l)
+# de la grilla (grid) que no se salen de la misma
+# al regar con un aspersor de radio r
+
 C1_hl = {}
 
 for r in radios:
     C1_hl[r] = [(f, c) for f in range(filas) for c in range(columnas) if
-               no_sale(f, c, r, filas, columnas)]
+                no_sale(f, c, r, filas, columnas)]
 
-
-# Lugares donde no puedo poner un regador excepto 
+# ------------------------------------------------------------------------------------------
+# Lugares donde no puedo poner un regador excepto
 # para distancias = r (para arriba, abajo, derecha, izquierda)
 
+# ----------------------------------------------------------------------------------------------
 # Todos los lugares que son regados si se pone un regador en (x, y)
 
 W_hlr = {}
 
 for r in radios:
-    for (x, y) in C_hl[r]:
+    for (x, y) in C1_hl[r]:
         W_hlr[(x, y, r)] = [(f, c) for f in range(filas) for c in range(columnas) if
-                         riega(x, y, f, c, r)]
+                            riega(x, y, f, c, r)]
+    
+# ---------------------------------------------------------------------------------------------
+# Numero de lugares que son regados si pongo un regador de radio r en (x, y)
 
 R_hlr = {}
 
@@ -41,22 +50,14 @@ for r in radios:
     for x, y in C1_hl[r]:
         R_hlr[(x, y, r)] = len(W_hlr[(x, y, r)])
 
-
-   
-#deberia incluir el radio en el diccionario? si
-
-print(C_hl)
-print()
-for (x, y) in W_hlr:
-    print((x, y), W_hlr[(x, y)])
-
-
+# ---------------------------------------------------------------------------------------------
 
 # Todos los lugares donde si se coloca un aspersor en (x,y) se hacen vecinos
-# Falta
-# Son vecinos si comparten pasto a regar
+
 
 V_xyr = {}
 
 for r in radios:
     for x, y in C1_hl[r]:
+        V_xyr[(x, y, r)] = vecino(x, y, r, filas, columnas)
+
--- auxiliares.py
+++ auxiliares.py
@@ -8,12 +8,32 @@ def no_sale(f, c, r, filas, columnas):
     else:
         return False
 
+
 def riega(x, y, f, c, r):
     if x + r >= f and y + r >= c and x - r <= f and y - r <= c and (x, y) != (f, c):
         return True
     else:
         return False
-    
-def vecino(x, y, f, c, r):
-    pass
 
+
+def vecino(f, c, r, fil, col):
+    vec = []
+    opciones = []
+    # 1er vecino:
+    primero = (f, c + r + 1)
+    opciones.append(primero)
+    # 2do vecino
+    segundo = (f, c - r - 1)
+    opciones.append(segundo)
+    # 3ro vecino
+    tercero = (f - r - 1, c)
+    opciones.append(tercero)
+    # 4to vecino
+    cuarto = (f + 1 + r, c)
+    opciones.append(cuarto)
+
+    for x, y in opciones:
+        if no_sale(x, y, r, fil, col):
+            vec.append((x, y))
+    
+    return vec
